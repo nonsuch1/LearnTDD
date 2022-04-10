bfe33f727ba61d055a97f946bea9dbbcb989199f
nonsuch1 <nonsuch.11111@gmail.com>
2022-04-10 19:29:15 +0300

    Removing subclasses

---
+++ src/main/java/com/nonsuch/model/Money.java
@@ -0,0 +1,37 @@
+package com.nonsuch.model;
+
+public class Money {
+    protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    };
+
+    public String currency() {
+        return currency;
+    }
+
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    public static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+
+    public boolean equals (Object object) {
+        Money money = (Money) object;
+        return amount == money.amount
+                && currency().equals(money.currency());
+    }
+
+    public String toString() {
+        return amount + " " + currency;
+    }
+}
---
+++ src/test/java/com/nonsuch/Test1.java
@@ -0,0 +1,30 @@
+package com.nonsuch;
+
+import com.nonsuch.model.Money;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class Test1 {
+    @Test
+    public void testMultiplication() {
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
+    }
+
+    @Test
+    public void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+    }
+}
